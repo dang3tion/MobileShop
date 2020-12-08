000a36a3a55664d660464ede1d9a16c70cd88507
Bảo Bảo <44540022+Thanh-Bao@users.noreply.github.com>
2020-12-08 17:38:07 +0700

    update

--- src/model/utility/Config.java
+++ src/model/utility/Config.java
@@ -2,25 +2,26 @@ package model.utility;
 
 public class Config {
 
+	
 //	   _____    ____    _           ____            _   _                
 //	   / ____|  / __ \  | |         / __ \          | | (_)               
 //	  | (___   | |  | | | |        | |  | |  _ __   | |  _   _ __     ___ 
 //	   \___ \  | |  | | | |        | |  | | | '_ \  | | | | | '_ \   / _ \
 //	   ____) | | |__| | | |____    | |__| | | | | | | | | | | | | | |  __/
 //	  |_____/   \___\_\ |______|    \____/  |_| |_| |_| |_| |_| |_|  \___|
-//	___________________________________________________________________________________________  ______________________________________________________ ______________________________________________________ ______________________________________________________ ____________________________________________________________________________________________________                                                                  
-//	public static final String DBDRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-//	public static final String SERVER_NAME = "jdbc:sqlserver://mobileshop.civgzrydcxzm.ap-southeast-1.rds.amazonaws.com:1433";
-//	public static final String DATABASE_NAME = "MOBILESHOP";
-//	public static final String USERNAME_DB = "bao";
-//	public static final String PASSWORD_DB = "12345678";
+//	___________________________________________________________________________________________  ______________________________________________________ ______________________________________________________ ___________________________________________________________________________________________________________________________________________________                                                                  
+	public static final String DBDRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+	public static final String SERVER_NAME = "jdbc:sqlserver://mobileshop.civgzrydcxzm.ap-southeast-1.rds.amazonaws.com:1433";
+	public static final String DATABASE_NAME = "MOBILESHOP";
+	public static final String USERNAME_DB = "bao";
+	public static final String PASSWORD_DB = "12345678";
+//	____________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
 //	#####public static final String STRING_CONNECTION = "jdbc:sqlserver://localhost:1433;DATABASE_NAME=Demo;user=sa;password=sa";
 
-	// for Send
-	// Mail_________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+	// for Send Mail OTP_________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
 	public static final String OWNER_EMAIL = "lazadamimax3@gmail.com";
 	public static final String MAIL_PASSWORD = "Admin123@#";
-
+//	___________________________________________________________________________________________________________________________________________________________________________________________________
 	// Login bằng google
 	private static final String LINK_WEB = "localhost:8080/MobileShop";
 	
@@ -41,10 +42,10 @@ public class Config {
 //	  ____) | | |__| | | |____        | | | (_) | | (__  | (_| | | | | | | | | (_) | \__ \ | |_ 
 //	 |_____/   \___\_\ |______|       |_|  \___/   \___|  \__,_| |_| |_| |_|  \___/  |___/  \__|
 //	___________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
-	public static final String DBDRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-	public static final String SERVER_NAME = "jdbc:sqlserver://localhost:1433";
-	public static final String DATABASE_NAME = "Demo2";
-	public static final String USERNAME_DB = "sa";
-	public static final String PASSWORD_DB = "sa";
-
+//	public static final String DBDRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+//	public static final String SERVER_NAME = "jdbc:sqlserver://localhost:1433";
+//	public static final String DATABASE_NAME = "Demo2";
+//	public static final String USERNAME_DB = "sa";
+//	public static final String PASSWORD_DB = "sa";
+//	_______________________________________________________________________________________________________________________________________________________________________
 }
