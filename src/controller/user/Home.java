d4ec4dcbd3becce1a1091dfc80a494d5d02d6b80
Bảo Bảo <44540022+Thanh-Bao@users.noreply.github.com>
2020-12-09 21:02:33 +0700

    update

--- src/controller/user/Home.java
+++ src/controller/user/Home.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = "/index")
-public class Index extends HttpServlet {
+public class Home extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
