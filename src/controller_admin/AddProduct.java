3e3f2deec386693d21195fc8a834f347631ca41a
CuongNguyen13 <vancuong13032000@gmail.com>
2021-01-04 15:15:35 +0700

    tạo các hàm thêm trong add product

--- src/controller_admin/AddProduct.java
+++ src/controller_admin/AddProduct.java
@@ -15,6 +15,7 @@ import model_DAO.DAO_AddProduct;
 public class AddProduct extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO_AddProduct dao = new DAO_AddProduct();
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
@@ -25,24 +26,36 @@ public class AddProduct extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		String idProduct = dao.createIdProduct();
 		String name = request.getParameter("nameProduct");
 		String price = request.getParameter("price");
 		String priceSale = request.getParameter("priceSale");
-		String brand =  request.getParameter("brand");
+		String brand = request.getParameter("brand");
 		String quatity = request.getParameter("quatity");
 		String type = request.getParameter("type");
 		String state = request.getParameter("state");
 		String topic = request.getParameter("topic");
 		String countColor = request.getParameter("countColor");
 		for (int i = 1; i <= Integer.parseInt(countColor); i++) {
-			String id = dao.createIdColor();
-			String code = request.getParameter("codeColor"+i);
-			String nameColor = request.getParameter("color"+i);
-			dao.addColor(id, code, nameColor);	
+			//lấy mã màu từ jsp
+			String idColor = dao.createIdColor();
+			String code = request.getParameter("codeColor" + i);
+			String nameColor = request.getParameter("color" + i);
+			dao.addColor(idColor, code, nameColor);
+			//lấy hình ảnh từ jsp
+			String imgMain = request.getParameter("anhnen" + i);
+			String imgEx1 = request.getParameter("anhMT1" + i);
+			String imgEx2 = request.getParameter("anhMT2" + i);
+			String imgEx3 = request.getParameter("anhMT3" + i);
+			String imgEx4 = request.getParameter("anhMT4" + i);
+			dao.addImg(idProduct, idColor, imgMain, "NEN");
+			dao.addImg(idProduct, idColor, imgEx1, "PHU");
+			dao.addImg(idProduct, idColor, imgEx2, "PHU");
+			dao.addImg(idProduct, idColor, imgEx3, "PHU");
+			dao.addImg(idProduct, idColor, imgEx4, "PHU");
+
 		}
-		
-		
-		
+
 //		System.out.println(name);
 //		System.out.println(price);
 //		System.out.println(priceSale);
--- src/model_DAO/DAO_AddProduct.java
+++ src/model_DAO/DAO_AddProduct.java
@@ -7,6 +7,7 @@ import model_ConnectDB.ExecuteStatementUtility;
 import model_beans.Account;
 
 public class DAO_AddProduct extends ExecuteStatementUtility {
+	
 	public String createIdColor() {
 		String color = "";
 		String query = "select * from MAUSAC order by MAMAU desc";
@@ -31,9 +32,71 @@ public class DAO_AddProduct extends ExecuteStatementUtility {
 			e.printStackTrace();
 		}
 	}
+	
+	public String createIdProduct() {
+		String id = "";
+		String query = "select * from HINHANH order by MASP desc";
+		try (ResultSet rs = super.AccessDBstr(query)) {
+			if (rs.next()) {
+				id = rs.getString(1).replace("SP", "").trim();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "SP"+(Integer.parseInt(id)+1);
+	}
+	
+	public void addImg(String idProduct,String idColor,String linkImg,String type) {
+		try {
+			String query = "INSERT INTO HINHANH VALUES(?,?,?,?)";
+			String[] parameters = {idProduct,idColor, linkImg,type };
+
+			try (ResultSet rs = super.AccessDBstr(query, parameters)) {
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public String createIdBrand() {
+		String id = "";
+		String query = "select * from THUONGHIEU order by MATH desc";
+		try (ResultSet rs = super.AccessDBstr(query)) {
+			if (rs.next()) {
+				id = rs.getString(1).replace("TH", "").trim();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return "TH"+(Integer.parseInt(id)+1);
+	}
+	
+	public String idBrand(String name) {
+		String idBrand = createIdBrand();
+		String query = "select * from THUONGHIEU";
+		try (ResultSet rs = super.AccessDBstr(query)) {
+			while (rs.next()) {
+				if (name.equals(rs.getString("TENTH"))) {
+					return idBrand = rs.getString("MATH");
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			String query1 = "INSERT INTO THUONGHIEU VALUES(?,?,?,?)";
+			String[] parameters = {idBrand,name,"0","Còn kinh doanh"};
+			try (ResultSet rs = super.AccessDBstr(query1, parameters)) {
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return idBrand;
+	}
+	
 	public static void main(String[] args) {
 		DAO_AddProduct dao = new DAO_AddProduct();
-		System.out.println(dao.createIdColor());
+		System.out.println(dao.idBrand("HH"));
 	}
 	
 }
