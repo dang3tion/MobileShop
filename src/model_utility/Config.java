13c054e5b75c50183861a0b9d1a84c4c08e32d2d
Bảo Bảo <Thanh-Bao@outlook.com>
2021-01-25 10:54:41 +0700

    fix login with google

--- src/model_utility/Config.java
+++ src/model_utility/Config.java
@@ -24,7 +24,7 @@ public class Config {
 //	  ____) | | |__| | | |____        | | | (_) | | (__  | (_| | | | | | | | | (_) | \__ \ | |_ 
 //	 |_____/   \___\_\ |______|       |_|  \___/   \___|  \__,_| |_| |_| |_|  \___/  |___/  \__|
 //	___________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
-	public static final String SERVER_NAME = "jdbc:sqlserver://localhost:1433";
+	public static final String SERVER_NAME = "jdbc:sqlserver://103.130.212.219:1433";
 	public static final String DATABASE_NAME = "MOBILESHOP";
 	public static final String USERNAME_DB = "sa";
 	public static final String PASSWORD_DB = "sa";
@@ -46,8 +46,10 @@ public class Config {
 
 	public static String GOOGLE_CLIENT_ID = "513013153147-13nhsbm8a1gtp4i1vq2l9j228f1q8691.apps.googleusercontent.com";
 	public static String GOOGLE_CLIENT_SECRET = "9vMjUVMMfYB3PfMxU4FD6heb";
-	public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/MobileShop/login-google";
-	public static String GOOGLE_URL_HREF_JSP = "https://accounts.google.com/o/oauth2/auth?scope=email&redirect_uri=http://localhost:8080/MobileShop/login-google&response_type=code&client_id=513013153147-13nhsbm8a1gtp4i1vq2l9j228f1q8691.apps.googleusercontent.com&approval_prompt=force";
+	public static String GOOGLE_REDIRECT_URI = "https://mobileshop.tk/login-google";
+	public static String GOOGLE_URL_HREF_JSP = "https://accounts.google.com/o/oauth2/auth?scope=email&redirect_uri="
+			+ GOOGLE_REDIRECT_URI
+			+ "&response_type=code&client_id=513013153147-13nhsbm8a1gtp4i1vq2l9j228f1q8691.apps.googleusercontent.com&approval_prompt=force";
 
 	public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
 	public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
@@ -60,21 +62,15 @@ public class Config {
 	// tên token ghi nhớ đăng nhập lưu trong cookie browser
 	public static final String NAME_TOKEN_REMEMBER_LOGIN = "0h5rg67";
 	public static final int MAX_TIME_REMEMBER_LOGIN = 1000 * 3600 * 24 * 30;
-	
-	
-	
-	// recaptcha google 
-	   public static final String SITE_KEY ="6LelZAsTAAAAAAv1ADYDnq8AzbmPmbMvjh-xhfgB";
-	   public static final String SECRET_KEY ="6LelZAsTAAAAAKa_s6g2yuJzByYlk0FH_6iTkZJC";
-	   
-	   
+
+	// recaptcha google
+	public static final String SITE_KEY = "6LelZAsTAAAAAAv1ADYDnq8AzbmPmbMvjh-xhfgB";
+	public static final String SECRET_KEY = "6LelZAsTAAAAAKa_s6g2yuJzByYlk0FH_6iTkZJC";
+
 	// Giới hạn số lượng sản phẩm giỏ hàng
-	   public static final int MAX_PRODUCT = 5 ;
-	   public static final int MAX_QUANTITY_OF_PRODUCT = 5 ;
-	   
+	public static final int MAX_PRODUCT = 5;
+	public static final int MAX_QUANTITY_OF_PRODUCT = 5;
 
-
-	   
 }
 
 //____                 ____                           _______ _                      
